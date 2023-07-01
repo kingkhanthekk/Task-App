468bb2d7102d1dc5601c4b68e37b95ec75f5c269
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-01 15:59:04 +0300

    First test case for user sign up.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,2 +1,13 @@
 const app = require("../index");
 const request = require("supertest");
+
+test("Should sign up a user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      username: "mojitel",
+      email: "abulk690@gmail.com",
+      password: "amijinis",
+    })
+    .expect(200);
+});
