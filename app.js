53afebc0392ba78bb021b9304740501644825bec
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-01 15:39:52 +0300

    Reconstructed codes to work with test cases using supertest.

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-//node modules
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-
-const userRoutes = require("./routers/user");
-const taskRoutes = require("./routers/task");
-
-//mongoDB connection
-mongoose
-  .connect("mongodb://127.0.0.1:27017/taskAppDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database Connected");
-  })
-  .catch(() => {
-    console.log("Database error!");
-  });
-
-//app use methods
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//routers
-app.use("/users", userRoutes);
-app.use("/tasks", taskRoutes);
+const app = require("./index");
 
 //app start
 const port = process.env.PORT || 3000;
